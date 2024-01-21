08c9e136cfc5478c4d25be9837f8eef8d3d6ceb8
Sophie Haber <SHaber@lcs.on.ca>
2024-01-21 14:08:31 -0500

    Initial Commit

---
+++ TradingCard/TradingCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradingCardApp.swift
+//  TradingCard
+//
+//  Created by Sophie Haber on 21.01.24.
+//
+
+import SwiftUI
+
+@main
+struct TradingCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
